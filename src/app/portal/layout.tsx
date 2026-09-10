fa2f94060ef11b92bda1d96d5d95cab993584554
Forged-Digital <pws.aaron@gmail.com>
2026-09-10 14:38:41 -0500

    Add billing shortcut to client portal

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import "./portal-live.css";
+import "./layout.css";
 
 export default function PortalLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return <><Link className="portal-billing-shortcut" href="/portal/billing">BILLING & INVOICES</Link>{children}</>;
 }
